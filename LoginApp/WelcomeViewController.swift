d7bbbca990767c2e6b9932ed0dbb992ceb155928
ISkvor <starlingy608@gmail.com>
2022-01-11 01:15:29 +0300

    setup storyboard

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Юрий Скворцов on 11.01.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
